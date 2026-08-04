19e257c6242fb7917ebd246c8c7eef83fc6fe53e
zhenghaoz <zhangzhenghao@hotmail.com>
2026-08-05 00:35:00 +0800

    chore: remove empty package docs

--- internal/core/snowball/doc.go
+++
@@ -1,6 +0,0 @@
-// Package snowball contains the Snowball 3.1.1 Go execution environment used
-// by the generated stemming algorithms in child packages.
-//
-// The runtime and generated algorithms are vendored from Snowball commit
-// cd195b51e948a902a4312f023f4a14392516a543 under its BSD-3-Clause license.
-package snowball
