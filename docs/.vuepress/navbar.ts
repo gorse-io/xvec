de68e5d4192f852f0186e4926ba1c2a6685f83e3
zhenghaoz <zhangzhenghao@hotmail.com>
2026-08-30 18:47:46 +0800

    docs: remove Reference navbar menu

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -5,11 +5,4 @@ export default navbar([
   { text: "Collections", link: "/collection.html" },
   { text: "Queries", link: "/query.html" },
   { text: "Full-text search", link: "/full-text-search.html" },
-  {
-    text: "Reference",
-    children: [
-      { text: "Go API", link: "https://pkg.go.dev/github.com/gorse-io/xvec" },
-      { text: "GitHub", link: "https://github.com/gorse-io/xvec" },
-    ],
-  },
 ]);
