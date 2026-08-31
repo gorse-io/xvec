e41b494571f42021768085bdc940a5a38bcaaf40
zhenghaoz <zhangzhenghao@hotmail.com>
2026-08-31 18:28:22 +0800

    test(math): merge SIMD FHT tests

--- internal/ailego/math/fht_test.go
+++ internal/ailego/math/fht_test.go
@@ -15,11 +15,60 @@
 package mathutil
 
 import (
+	"math/rand/v2"
 	"slices"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
+
+func testFHTKernels(t *testing.T, kernels fhtKernels) {
+	t.Helper()
+
+	for _, size := range []int{1, 2, 3, 7, 8, 15, 16, 31, 32, 63, 64, 65, 127, 128} {
+		t.Run(strconv.Itoa(size), func(t *testing.T) {
+			input := make([]float32, size)
+			for index := range input {
+				input[index] = rand.Float32()*20 - 10
+			}
+
+			signs := make([]byte, (size+7)/8)
+			for index := range signs {
+				signs[index] = byte(rand.Uint32())
+			}
+			wantSigns := slices.Clone(input)
+			fhtFlipSignsScalar(signs, wantSigns)
+			gotSigns := slices.Clone(input)
+			kernels.flipSigns(signs, gotSigns)
+			require.Equal(t, wantSigns, gotSigns)
+
+			wantWalk := slices.Clone(input)
+			fhtKacWalkScalar(wantWalk)
+			gotWalk := slices.Clone(input)
+			kernels.kacWalk(gotWalk)
+			require.Equal(t, wantWalk, gotWalk)
+
+			wantInverse := slices.Clone(wantWalk)
+			fhtInverseKacWalkScalar(wantInverse)
+			gotInverse := slices.Clone(gotWalk)
+			kernels.inverseKacWalk(gotInverse)
+			require.Equal(t, wantInverse, gotInverse)
+		})
+	}
+
+	for _, size := range []int{1, 2, 4, 8, 16, 32, 64, 128, 256} {
+		input := make([]float32, size)
+		for index := range input {
+			input[index] = rand.Float32()*20 - 10
+		}
+		want := slices.Clone(input)
+		fhtInPlaceScalar(want)
+		got := slices.Clone(input)
+		kernels.inPlace(got)
+		require.Equal(t, want, got)
+	}
+}
 
 func TestFHTInPlace(t *testing.T) {
 	t.Parallel()
