6a37042bd2fc117fef9cddd9432efba6e9f4bbcb
zhenghaoz <zhangzhenghao@hotmail.com>
2026-09-05 01:14:18 +0800

    feat(rabitq): add space_excode NEON kernels

---
+++ pkg/rabitq/simd/space_excode_arm64_test.go
@@ -0,0 +1,39 @@
+//go:build !noasm && arm64
+
+// Copyright 2026-present the xvec project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package simd
+
+import (
+	"testing"
+
+	"golang.org/x/sys/cpu"
+)
+
+func TestSpaceExcodeNEON(t *testing.T) {
+	if !cpu.ARM64.HasASIMD {
+		t.Skip("ASIMD is not supported")
+	}
+	testSpaceExcode(t, []excodeIPFunc{
+		ip16_fxu1_neon,
+		ip64_fxu2_neon,
+		ip64_fxu3_neon,
+		ip16_fxu4_neon,
+		ip64_fxu5_neon,
+		ip64_fxu6_neon,
+		ip64_fxu7_neon,
+		ip16_fxu8_neon,
+	})
+}
