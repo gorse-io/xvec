4d91ecee3072955dfcdb97f218bce3b93f1297a3
zhenghaoz <zhangzhenghao@hotmail.com>
2026-09-05 08:07:17 +0800

    test(rabitq): remove redundant warmup coverage

--- pkg/rabitq/simd/warmup_test.go
+++ pkg/rabitq/simd/warmup_test.go
@@ -30,44 +30,6 @@ func TestWarmupDispatch(t *testing.T) {
 	testWarmup(t, warmupIPX0Q512)
 }
 
-func TestWarmupIPX0Q512(t *testing.T) {
-	for _, dimension := range []int{64, 448, 512, 576, 1024} {
-		for _, queryBits := range []int{1, 4, 8} {
-			data, query := makeWarmupInput(dimension, queryBits)
-			const delta = float32(0.25)
-			const vl = float32(-0.75)
-			want := warmupOracle(data, query, delta, vl, dimension, queryBits)
-			got := WarmupIPX0Q512(data, query, delta, vl, dimension, queryBits)
-			require.Equal(t, want, got, "dimension %d query bits %d", dimension, queryBits)
-		}
-	}
-}
-
-func TestWarmupIPX0Q512RejectsInvalidInput(t *testing.T) {
-	data, query := makeWarmupInput(64, 8)
-	tests := []struct {
-		name      string
-		data      []uint64
-		query     []uint64
-		dimension int
-		queryBits int
-	}{
-		{name: "zero dimension", data: data, query: query, queryBits: 8},
-		{name: "unaligned dimension", data: data, query: query, dimension: 63, queryBits: 8},
-		{name: "zero query bits", data: data, query: query, dimension: 64},
-		{name: "too many query bits", data: data, query: query, dimension: 64, queryBits: 9},
-		{name: "short data", query: query, dimension: 64, queryBits: 8},
-		{name: "short query", data: data, dimension: 64, queryBits: 8},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			require.Panics(t, func() {
-				WarmupIPX0Q512(test.data, test.query, 1, 1, test.dimension, test.queryBits)
-			})
-		})
-	}
-}
-
 func testWarmup(t *testing.T, kernel func(unsafe.Pointer, unsafe.Pointer, float32, float32, int64, int64) float32) {
 	t.Helper()
 	coefficients := [][2]float32{
